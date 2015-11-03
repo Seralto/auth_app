35e17ab74aead19871175187cc4cdb5a703e20e0
Sérgio Toledo <sergio.x.toledo.-nd@espn.com>
2015-11-03 17:57:42 -0200

    Added login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def signin
+  end
+
+  def login
+    @user = User.where(email: params[:user][:email]).first
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to '/signin'
+    end 
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to '/signin'
+  end
+
   private
     def user_params
       params.required(:user).permit(:first_name, :last_name, :email, :password)
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   resources :users
   get 'signup' => 'users#new'
+  get 'signin' => 'users#signin'
+
+  post 'signin' => 'users#login'
+  get 'logout' => 'users#logout'
 end
